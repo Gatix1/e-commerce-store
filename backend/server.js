7fafa88279cdb59fe986fc04d726ec02720c50f2
Maxim Pintea <maxim.pintea7@gmail.com>
2025-05-13 16:33:35 +0200

    setted up mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,4 +11,7 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+    connectDB();
+});
